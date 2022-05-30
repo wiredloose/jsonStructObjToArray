0e9965b31e66ed460ec0a722369387c390072375
Ewan Andreasen <ewan.andreasen@danfoss.com>
2022-05-30 19:32:41 +0200

    generalized the function and added query params.

--- jsonStructObjToArray/ObjectPropsToArray.cs
+++ jsonStructObjToArray/ObjectPropsToArray.cs
@@ -18,36 +18,47 @@ namespace jsonStructObjToArray
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
             string json = req.Query["json"];
             string jpath = req.Query["jpath"]; // JPath expressions, see https://www.newtonsoft.com/json/help/html/SelectToken.htm 
-            
+            string keyToProp = req.Query["keyToProp"];
+            string valToProp = req.Query["valToProp"];
+
             string jsonStr = string.IsNullOrEmpty(json)
                 ? "{\"Category\":\"Azure Functions\",\"Action\":\"Run\",\"Label\":\"Test\"}"
                 : json;
             string jpathStr = string.IsNullOrEmpty(jpath)
                 ? "result.watts"
                 : jpath;
-            
+            string keyToPropStr = string.IsNullOrEmpty(keyToProp)
+                ? "propName"
+                : keyToProp;
+            string valToPropStr = string.IsNullOrEmpty(valToProp)
+                ? "propValue"
+                : valToProp;
+
             dynamic jObject = JsonConvert.DeserializeObject(jsonStr);
 
-            //Modify JSON object
+            // Locate JSON object to be modified
             JObject jObj = jObject;
             JToken jpathObj = jObj.SelectToken(jpathStr);
             JObject parsedJson = JObject.Parse(jpathObj.ToString());
-            
+
+            // Move all properties to array of objects 
+            JArray jToArr = new JArray();
             foreach (var prop in parsedJson.Properties())
             {
-                
-                log.LogInformation(jpathStr + " property name: " + prop.Name +" with value: " + prop.Value);
+                jToArr.Add(
+                    new JObject(
+                        new JProperty(keyToPropStr, prop.Name),
+                        new JProperty(valToPropStr, prop.Value)
+                        )
+                    );
             }
 
-            //jpathObj.Remove();
-            JObject jResultObj = (JObject)jObj.SelectToken("result");
-            log.LogInformation("removing watts successfully?: " + jResultObj.Remove("watts"));
-            log.LogInformation("jObj after removal: " + jObj.ToString());
-            log.LogInformation("parsedJson after removal: " + parsedJson.ToString());
+            // Replace property object with version that is array of objects
+            JObject jForReplaceObj = (JObject)jpathObj;
+            JProperty jForReplaceProp = (JProperty)jForReplaceObj.Parent;
+            jForReplaceObj.Parent.Replace( new JProperty(jForReplaceProp.Name, jToArr) );
 
             //return new OkObjectResult(jObject);
             return new OkObjectResult(jObj);
